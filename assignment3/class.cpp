f99bcd1531f86263e87eeae438efd24f95e45afb
sonder037 <whuzjchang@gmail.com>
2025-12-04 20:57:02 +0800

    feat:完成assignment3

---
+++ assignment3/class.cpp
@@ -0,0 +1,10 @@
+#include "class.h"
+
+void mClass::setData(int message) {
+    this->data = message;
+}
+
+int mClass::getData() {
+    return this->data;
+}
+
---
+++ assignment3/class.h
@@ -0,0 +1,9 @@
+class mClass {
+public:
+    mClass();
+    mClass(int x) : data(x) {}
+    int getData();
+    void setData(int message);
+private:
+    int data;
+};
